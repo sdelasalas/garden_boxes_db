f934f5798804e2eae7d94015017f31df5f91afd2
Stacy <stacy@Alderaan.local>
2020-04-02 13:44:24 -0400

    takes in length and width

--- garden_boxes_sqlite/garden_boxes_sqlite/Program.cs
+++ garden_boxes_sqlite/garden_boxes_sqlite/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using Mono.Data.Sqlite;
 
 namespace garden_boxes_sqlite
 {
@@ -6,7 +8,27 @@ namespace garden_boxes_sqlite
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create and open a db connection
+            string connectionString = @"Data Source=/Users/stacy/Projects/gardenbox_db/garden-boxes/database.sqlite";
+            SqliteConnection connection = new SqliteConnection(connectionString);
+            //connection.Open();
+            connection.Open();
+
+            Console.WriteLine("Welcome to Garden Boxes!");
+
+            //ask length and width
+            string length;
+            Console.WriteLine("Enter the length of your box: ");
+            length = Console.ReadLine();
+            int userIntLength = Convert.ToInt32(length);
+
+            string width;
+            Console.WriteLine("Enter the width of your box: ");
+            width = Console.ReadLine();
+            int userIntWidth = Convert.ToInt32(width);
+
+            connection.Close();
         }
+        
     }
 }
