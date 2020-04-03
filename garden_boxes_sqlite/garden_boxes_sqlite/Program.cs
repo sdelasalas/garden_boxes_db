77466f3ff00aa1561c37150fc5e35a7b2ce90a12
Stacy <stacy@Alderaan.local>
2020-04-02 21:51:11 -0400

    carrots calculate correctly using db

--- garden_boxes_sqlite/garden_boxes_sqlite/Program.cs
+++ garden_boxes_sqlite/garden_boxes_sqlite/Program.cs
@@ -20,28 +20,60 @@ namespace garden_boxes_sqlite
             string length;
             Console.WriteLine("Enter the length of your box: ");
             length = Console.ReadLine();
-            int userIntLength = Convert.ToInt32(length);
+            int @userIntLength = Convert.ToInt32(length);
 
             string width;
             Console.WriteLine("Enter the width of your box: ");
             width = Console.ReadLine();
-            int userIntWidth = Convert.ToInt32(width);
+            int @userIntWidth = Convert.ToInt32(width);
             
-            Console.WriteLine("Your garden box area is: " + userIntLength * userIntWidth + " and the perimeter of your box is: " + ((2 * userIntLength) + (2 * userIntWidth)) + ".");
+            Console.WriteLine("Your garden box area is: " + @userIntLength * @userIntWidth + " and the perimeter of your box is: " + ((2 * @userIntLength) + (2 * @userIntWidth)) + ".");
            
 
             //use database to read back that number of veggies that can be planted
             while (true)
             {
                 //present a menu of vegetables to plant
-                Console.WriteLine("What vegetable would you like to plant? (Please select letter.)");
-                Console.WriteLine("a.  Carrots");
-                Console.WriteLine("b.  Corn");
-                Console.WriteLine("c.  Beets");
-                Console.WriteLine("d.  Quit");
+                Console.WriteLine("What vegetable would you like to plant? Or choose Quit to End");
+                Console.WriteLine("Carrots");
+                Console.WriteLine("Corn");
+                Console.WriteLine("Beets");
+                Console.WriteLine("Quit");
 
                 //get answer choice and convert to lower
                 string userChoice = Console.ReadLine().ToLower();
+
+                if (userChoice == "carrots")
+                {
+                    Console.WriteLine("You picked Carrots!");
+                    //Get Carrot calc from data base
+                    string sql = $"SELECT PlantPoss FROM Veggies WHERE SeedType = '{userChoice}'";
+                    SqliteCommand command = new SqliteCommand(sql, connection);
+                    SqliteDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                        int plantPoss = Convert.ToInt32(reader["PlantPoss"]);
+                    Console.WriteLine($"You can plant " + (plantPoss * (userIntLength * userIntWidth)) + "carrots in your garden!");
+                    reader.Close();
+                }
+
+                else if (userChoice == "corn")
+                {
+                    //SELECT Corn from DB
+                }
+
+                else if (userChoice == "beets")
+                {
+                    //SELECT Beets from DB
+                }
+
+                else if (userChoice == "quit")
+                {
+                    //while = false
+                }
+
+                else
+                    Console.WriteLine("I don't know what  " + userChoice + " are. Please enter another.");
+
             }
 
 
